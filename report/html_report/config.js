43c7a546961c8dae133ec7610c823187c548e684
Taras <taraskendzor@gmail.com>
2019-05-14 11:43:49 +0300

    deploy static content

---
+++ report/html_report/config.js
@@ -0,0 +1,24 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_test_0_body_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190514-114102\\test_test_0_body_0_tablet_h.png",
+        "selector": "body",
+        "fileName": "test_test_0_body_0_tablet_h.png",
+        "label": "test",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/colored-blocks/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080",
+        "error": "Test file not found C:\\Users\\TARAS\\Documents\\GitHub\\layout_colored-blocks\\backstop_data\\bitmaps_test\\20190514-114102\\test_test_0_body_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
